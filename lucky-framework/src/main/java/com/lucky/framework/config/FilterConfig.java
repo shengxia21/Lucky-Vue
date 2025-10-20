c6d215f8325d9729d002f84ae5ee9aa15aa96e9e
浮生絮语 <1710647252@qq.com>
2025-10-20 22:37:24 +0800

    支持防盗链功能

--- lucky-framework/src/main/java/com/lucky/framework/config/FilterConfig.java
+++ lucky-framework/src/main/java/com/lucky/framework/config/FilterConfig.java
@@ -1,17 +1,19 @@
 package com.lucky.framework.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.lucky.common.constant.Constants;
+import com.lucky.common.filter.RefererFilter;
+import com.lucky.common.filter.RepeatableFilter;
+import com.lucky.common.filter.XssFilter;
+import com.lucky.common.utils.StringUtils;
 import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.lucky.common.filter.RepeatableFilter;
-import com.lucky.common.filter.XssFilter;
-import com.lucky.common.utils.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Filter配置
@@ -27,6 +29,9 @@ public class FilterConfig {
     @Value("${xss.urlPatterns}")
     private String urlPatterns;
 
+    @Value("${referer.allowed-domains}")
+    private String allowedDomains;
+
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Bean
     @ConditionalOnProperty(value = "xss.enabled", havingValue = "true")
@@ -37,8 +42,25 @@ public class FilterConfig {
         registration.addUrlPatterns(StringUtils.split(urlPatterns, ","));
         registration.setName("xssFilter");
         registration.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
-        Map<String, String> initParameters = new HashMap<String, String>();
+        Map<String, String> initParameters = new HashMap<>();
         initParameters.put("excludes", excludes);
+        registration.setInitParameters(initParameters);
+        return registration;
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Bean
+    @ConditionalOnProperty(value = "referer.enabled", havingValue = "true")
+    public FilterRegistrationBean refererFilterRegistration()
+    {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setDispatcherTypes(DispatcherType.REQUEST);
+        registration.setFilter(new RefererFilter());
+        registration.addUrlPatterns(Constants.RESOURCE_PREFIX + "/*");
+        registration.setName("refererFilter");
+        registration.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("allowedDomains", allowedDomains);
         registration.setInitParameters(initParameters);
         return registration;
     }
