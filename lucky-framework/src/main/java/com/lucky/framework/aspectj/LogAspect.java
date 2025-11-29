8ebe4acf60eb95342d2aae4eedaa2643a6303482
浮生絮语 <1710647252@qq.com>
2025-11-30 03:26:33 +0800

    日志排除apiKey字段属性

--- lucky-framework/src/main/java/com/lucky/framework/aspectj/LogAspect.java
+++ lucky-framework/src/main/java/com/lucky/framework/aspectj/LogAspect.java
@@ -1,22 +1,5 @@
 package com.lucky.framework.aspectj;
 
-import java.util.Collection;
-import java.util.Map;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.ArrayUtils;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.NamedThreadLocal;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.multipart.MultipartFile;
 import com.alibaba.fastjson2.JSON;
 import com.lucky.common.annotation.Log;
 import com.lucky.common.core.domain.entity.SysUser;
@@ -33,6 +16,23 @@ import com.lucky.common.utils.ip.IpUtils;
 import com.lucky.framework.manager.AsyncManager;
 import com.lucky.framework.manager.factory.AsyncFactory;
 import com.lucky.system.domain.SysOperLog;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.ArrayUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * 操作日志记录处理
@@ -46,7 +46,7 @@ public class LogAspect {
     /**
      * 排除敏感属性字段
      */
-    public static final String[] EXCLUDE_PROPERTIES = {"password", "oldPassword", "newPassword", "confirmPassword"};
+    public static final String[] EXCLUDE_PROPERTIES = {"apiKey", "password", "oldPassword", "newPassword", "confirmPassword"};
     private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
     /**
      * 计算操作消耗时间
