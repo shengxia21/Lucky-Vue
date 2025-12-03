218c46935c1fa6216c498b541ba920d36b46684e
浮生絮语 <1710647252@qq.com>
2025-12-04 04:09:35 +0800

    新增mybatis Map<String, Object>字段类型的转换器

---
+++ lucky-common/src/main/java/com/lucky/common/handler/StringMapTypeHandler.java
@@ -0,0 +1,63 @@
+package com.lucky.common.handler;
+
+import com.alibaba.fastjson2.JSON;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+import org.apache.ibatis.type.TypeHandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * Map<String, Object> 的类型转换器实现类，对应数据库的 varchar 类型
+ *
+ * @author lucky
+ */
+@MappedJdbcTypes(JdbcType.VARCHAR)
+@MappedTypes(Map.class)
+public class StringMapTypeHandler implements TypeHandler<Map<String, Object>> {
+
+    @Override
+    public void setParameter(PreparedStatement ps, int i, Map<String, Object> parameter, JdbcType jdbcType) throws SQLException {
+        if (parameter == null) {
+            ps.setString(i, null);
+        } else {
+            String json = JSON.toJSONString(parameter);
+            ps.setString(i, json);
+        }
+    }
+
+    @Override
+    public Map<String, Object> getResult(ResultSet rs, String columnName) throws SQLException {
+        String value = rs.getString(columnName);
+        return parseJson(value);
+    }
+
+    @Override
+    public Map<String, Object> getResult(ResultSet rs, int columnIndex) throws SQLException {
+        String value = rs.getString(columnIndex);
+        return parseJson(value);
+    }
+
+    @Override
+    public Map<String, Object> getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String value = cs.getString(columnIndex);
+        return parseJson(value);
+    }
+
+    private Map<String, Object> parseJson(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        try {
+            return JSON.parseObject(value);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
