f8b9cdb4a9980b2f7069d8d83e049aeec38e6717
浮生絮语 <1710647252@qq.com>
2026-04-16 22:01:27 +0800

    更新sql操作工具类

--- lucky-common/src/main/java/com/lucky/common/utils/sql/SqlUtil.java
+++ lucky-common/src/main/java/com/lucky/common/utils/sql/SqlUtil.java
@@ -14,10 +14,12 @@ public class SqlUtil {
      * 限制orderBy最大长度
      */
     private static final int ORDER_BY_MAX_LENGTH = 500;
+
     /**
      * 定义常用的 sql关键字
      */
-    public static String SQL_REGEX = "\u000B|and |extractvalue|updatexml|sleep|information_schema|exec |insert |select |delete |update |drop |count |chr |mid |master |truncate |char |declare |or |union |like |+|/*|user()";
+    public static String SQL_REGEX = "\u000B|%0A|and |extractvalue|updatexml|sleep|information_schema|exec |insert |select |delete |update |drop |count |chr |mid |master |truncate |char |declare |or |union |like |+|/*|user()";
+
     /**
      * 仅支持字母、数字、下划线、空格、逗号、小数点（支持多个字段排序）
      */
@@ -50,10 +52,11 @@ public class SqlUtil {
         if (StringUtils.isEmpty(value)) {
             return;
         }
+        String normalizedValue = value.replaceAll("\\p{Z}|\\s", "");
         String[] sqlKeywords = StringUtils.split(SQL_REGEX, "\\|");
         for (String sqlKeyword : sqlKeywords) {
-            if (StringUtils.indexOfIgnoreCase(value, sqlKeyword) > -1) {
-                throw new UtilException("参数存在SQL注入风险");
+            if (StringUtils.indexOfIgnoreCase(normalizedValue, sqlKeyword) > -1) {
+                throw new UtilException("请求参数包含敏感关键词'" + sqlKeyword + "'，可能存在安全风险");
             }
         }
     }
