5ed7a9cfbb8e3c6a45c46519eee9d8db4462d3d1
浮生絮语 <1710647252@qq.com>
2026-04-17 23:34:03 +0800

    用户密码支持自定义配置规则

--- lucky-admin/src/main/java/com/lucky/web/controller/system/SysLoginController.java
+++ lucky-admin/src/main/java/com/lucky/web/controller/system/SysLoginController.java
@@ -85,6 +85,7 @@ public class SysLoginController {
         ajax.put("user", user);
         ajax.put("roles", roles);
         ajax.put("permissions", permissions);
+        ajax.put("pwdChrtype", getSysAccountChrtype());
         ajax.put("isDefaultModifyPwd", initPasswordIsModify(user.getPwdUpdateDate()));
         ajax.put("isPasswordExpired", passwordIsExpiration(user.getPwdUpdateDate()));
         return ajax;
@@ -100,6 +101,11 @@ public class SysLoginController {
         Long userId = SecurityUtils.getUserId();
         List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
         return AjaxResult.success(menuService.buildMenus(menus));
+    }
+
+    // 获取用户密码自定义配置规则
+    public String getSysAccountChrtype() {
+        return Convert.toStr(configService.selectConfigByKey("sys.account.chrtype"), "0");
     }
 
     // 检查初始密码是否提醒修改
