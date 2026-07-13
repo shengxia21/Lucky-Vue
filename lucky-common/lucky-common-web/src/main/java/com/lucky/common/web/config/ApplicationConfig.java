a12caea44a6f81f33470a551a70d898117e0964f
浮生絮语 <1710647252@qq.com>
2026-07-13 22:43:37 +0800

    feat(web): 配置Java 8日期时间全局统一格式

--- lucky-common/lucky-common-web/src/main/java/com/lucky/common/web/config/ApplicationConfig.java
+++ lucky-common/lucky-common-web/src/main/java/com/lucky/common/web/config/ApplicationConfig.java
@@ -1,12 +1,19 @@
 package com.lucky.common.web.config;
 
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 /**
@@ -21,12 +28,26 @@ import java.util.TimeZone;
 @EnableAsync(proxyTargetClass = true)
 public class ApplicationConfig {
 
+    // 定义全局统一的日期时间格式
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+    private static final String TIME_PATTERN = "HH:mm:ss";
+
     /**
      * Jackson增强配置
      */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
         return builder -> {
+            // 1. 配置 LocalDateTime 序列化与反序列化
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
+            builder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
+            // 2. 配置 LocalDate 序列化与反序列化
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
+            builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
+            // 3. 配置 LocalTime 序列化与反序列化
+            builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern(TIME_PATTERN)));
+            builder.deserializers(new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TIME_PATTERN)));
             // 配置时区
             builder.timeZone(TimeZone.getDefault());
             // 将Long类型转换为字符串类型(解决雪花id在前端精度丢失问题)
